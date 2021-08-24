ec40938f5668e3202e13548724c0e48eee0999eb
Alex Kio <alexandermartelkio@gmail.com>
2021-08-24 17:13:46 -0400

    Import calendar array

---
+++ src/composables/time.js
@@ -0,0 +1,42 @@
+export const calendar = {
+  months: [
+    { number: 1, name: 'January' },
+    { number: 2, name: 'February' },
+    { number: 3, name: 'March' },
+    { number: 4, name: 'April' },
+    { number: 5, name: 'May' },
+    { number: 6, name: 'June' },
+    { number: 7, name: 'July' },
+    { number: 8, name: 'August' },
+    { number: 9, name: 'September' },
+    { number: 10, name: 'October' },
+    { number: 11, name: 'November' },
+    { number: 12, name: 'December' }
+  ],
+  hours: [
+    { number: 0, name: '12 AM' },  
+    { number: 1, name: '1 AM' },
+    { number: 2, name: '2 AM' },
+    { number: 3, name: '3 AM' },
+    { number: 4, name: '4 AM' },
+    { number: 5, name: '5 AM' },
+    { number: 6, name: '6 AM' },
+    { number: 7, name: '7 AM' },
+    { number: 8, name: '8 AM' },
+    { number: 9, name: '9 AM' },
+    { number: 10, name: '10 AM' },
+    { number: 11, name: '11 AM' },
+    { number: 12, name: '12 PM' },
+    { number: 13, name: '1 PM' },
+    { number: 14, name: '2 PM' },
+    { number: 15, name: '3 PM' },
+    { number: 16, name: '4 PM' },
+    { number: 17, name: '5 PM' },
+    { number: 18, name: '6 PM' },
+    { number: 19, name: '7 PM' },
+    { number: 20, name: '8 PM' },
+    { number: 21, name: '9 PM' },
+    { number: 22, name: '10 PM' },
+    { number: 23, name: '11 PM' }
+  ]
+}
