d267823cf5e84eb74d972198ac6f5159a857c124
Alex Kio <alexandermartelkio@gmail.com>
2021-08-24 14:48:03 -0400

    Create initial file structure

---
+++ src/components/Error.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Error</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Error',
+  components: {},
+});
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Possibilities.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Possibilities</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Possibilities',
+  components: {},
+});
+</script>
+
+<style>
+
+</style>
