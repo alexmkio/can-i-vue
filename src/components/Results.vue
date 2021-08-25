8eaf1344308803efc12637f0efb9379901d216c0
Alex Kio <alexandermartelkio@gmail.com>
2021-08-24 20:44:57 -0400

    Conditionally render notice object

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,5 +1,17 @@
 <template>
-  <h1>Results</h1>
+  <section className='results'>
+    <section v-if="nice">
+      <h2>Good news!</h2>
+      <p>You can go outside for {{ hours }} hours!</p>
+    </section>
+    <section v-else>
+      <h2>Bad news</h2>
+      <p>You can't go outside for {{ hours }} hours!</p>
+    </section>
+    <router-link to='/good_weather'>
+      See all the future good weather
+    </router-link>
+  </section>
 </template>
 
 <script lang="ts">
@@ -8,9 +20,20 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'Results',
   components: {},
-});
+  data() {
+    return {
+      hours: 666,
+      nice: true
+    }
+  },
+})
 </script>
 
 <style>
-
+.results {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1vh;
+}
 </style>
